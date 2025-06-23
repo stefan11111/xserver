fcf7fb1f42136685f0e5308e5f564ed8ad42e7c8
Mike Gelfand <mikedld@mikedld.com>
2025-06-23 22:43:05 +0100

    xfixes: move include guards up to wrap the whole file
    See: https://gcc.gnu.org/onlinedocs/cppinternals/Guard-Macros.html
    Signed-off-by: Mike Gelfand <mikedld@mikedld.com>

--- xfixes/xfixesint.h
+++ xfixes/xfixesint.h
@@ -42,12 +42,12 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifndef _XFIXESINT_H_
+#define _XFIXESINT_H_
+
 #ifdef HAVE_DIX_CONFIG_H
 #include <dix-config.h>
 #endif
-
-#ifndef _XFIXESINT_H_
-#define _XFIXESINT_H_
 
 #include <X11/X.h>
 #include <X11/Xproto.h>

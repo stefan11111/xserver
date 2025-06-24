003d73441b8478663233565c77ffe86bd88c464a
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-06-24 22:37:08 +0200

    present: Walk all children when setting window pixmaps
    We currently skip setting the window pixmap on any window
    not using its parent's pixmap. That does not work correctly
    in the presence of reparenting.
    
    Consider the following scenario:
    1. window A is created as child of B
    2. present starts flipping and sets the whole window
       tree to use pixmap X
    3. window C is created (uses the screen pixmap by default)
    4. window A is reparented to C
    5. present stops flipping and attempts to set the
       whole window tree back to the screen pixmap,
       except the walk terminates at window C
       since it's using an unexpected pixmap, and
       window A is left with the stale pixmap X
    6. pixmap X is destroyed
    7. the X server segfaults on a rendering operation
       on window A due the stale pixmap
    
    I managed to hit this with mpv (doing present flips)
    and crack-attack (keeps alternating between a menu
    window and an actual game window):
    1. start both applications
    2. start a game in crack-attack
    3. fullscreen mpv
    4. end the game in crack attack
    5. unfullscreen mpv
    6. the crack-attack menu window has appeared, but
       might be corrupted and doing stuff on it segfaults
       the X server
    
    I suppose the other option might be to make new windows
    automatically inherit their parent's pixmap instead
    of using the screen pixmap. But I've not looked into
    how that would affect eg. composite.
    
    Signed-off-by: notbabaisyou <though-went-some-simple@proton.me>

--- present/present.c
+++ present/present.c
@@ -120,9 +120,14 @@ present_set_tree_pixmap_visit(WindowPtr window, void *data)
     struct pixmap_visit *visit = data;
     ScreenPtr           screen = window->drawable.pScreen;
 
-    if ((*screen->GetWindowPixmap)(window) != visit->old)
-        return WT_DONTWALKCHILDREN;
-    (*screen->SetWindowPixmap)(window, visit->new);
+    if ((*screen->GetWindowPixmap)(window) == visit->old)
+        (*screen->SetWindowPixmap)(window, visit->new);
+
+    /*
+     * Walk the entire tree in case windows using the
+     * the old pixmap have been reparented to newly
+     * created windows using the screen pixmap instead.
+     */
     return WT_WALKCHILDREN;
 }
 

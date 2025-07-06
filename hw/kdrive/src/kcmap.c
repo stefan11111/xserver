a93cec5d080789c2c07f45b816f94560924c681c
stefan11111 <stefan11111@shitposting.expert>
2025-07-06 14:55:43 +0300

    kdrive: call newly-added recolorCursor cfunc
    In preparation for adding the Xfbdev X11 kdrive server
    
    Signed-off-by: stefan11111 <stefan11111@shitposting.expert>

--- hw/kdrive/src/kcmap.c
+++ hw/kdrive/src/kcmap.c
@@ -72,6 +72,11 @@ KdSetColormap(ScreenPtr pScreen)
     (*pScreenPriv->card->cfuncs->putColors) (pCmap->pScreen,
                                              (1 << pScreenPriv->screen->fb.
                                               depth), defs);
+
+
+    /* recolor hardware cursor */
+    if (pScreenPriv->card->cfuncs->recolorCursor)
+        (*pScreenPriv->card->cfuncs->recolorCursor) (pCmap->pScreen, 0, 0);
 }
 
 /*
@@ -231,4 +236,9 @@ KdStoreColors(ColormapPtr pCmap, int ndef, xColorItem * pdefs)
     }
 
     (*pScreenPriv->card->cfuncs->putColors) (pCmap->pScreen, ndef, pdefs);
+
+    /* recolor hardware cursor */
+    if (pScreenPriv->card->cfuncs->recolorCursor)
+        (*pScreenPriv->card->cfuncs->recolorCursor) (pCmap->pScreen, ndef,
+                                                     pdefs);
 }

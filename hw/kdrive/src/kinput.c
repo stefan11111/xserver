0a91e07ef4c4a42d5c4ba8698e1b11a83e59daae
stefan11111 <stefan11111@shitposting.expert>
2025-07-06 17:02:43 +0300

    kdrive: call the newly added SIGIO code
    The keyboard input driver that will be added to kdrive
    only works with old SIGIO code, and doesn't work with
    the new threaded input that kdrive uses.
    
    All the code is behind #if's that are never satisfied,
    so this commit has no functional changes.
    
    In preparation for adding the Xfbdev X11 kdrive server.
    
    Signed-off-by: stefan11111 <stefan11111@shitposting.expert>

--- hw/kdrive/src/kinput.c
+++ hw/kdrive/src/kinput.c
@@ -281,7 +281,11 @@ KdDisableInput(void)
     KdPointerInfo *pi;
     int found = 0, i = 0;
 
+#ifndef KDRIVE_KBD
     input_lock();
+#else
+    KdBlockSigio();
+#endif
 
     for (ki = kdKeyboards; ki; ki = ki->next) {
         if (ki->driver && ki->driver->Disable)
@@ -364,7 +368,11 @@ KdEnableInput(void)
         NoticeEventTime (&ev, pi->dixdev);
     }
 
+#ifndef KDRIVE_KBD
     input_unlock();
+#else
+    KdUnblockSigio();
+#endif
 }
 
 static KdKeyboardDriver *
@@ -1389,8 +1397,12 @@ KdInitInput(void)
     KdKeyboardInfo *ki;
     struct KdConfigDevice *dev;
 
+#ifndef KDRIVE_KBD
     if (kdConfigPointers || kdConfigKeyboards)
         InputThreadPreInit();
+#else
+    InputThreadEnable = FALSE;
+#endif
 
     kdInputEnabled = TRUE;
 
@@ -2085,9 +2097,17 @@ KdWakeupHandler(ScreenPtr pScreen, int result)
         if (pi->timeoutPending) {
             if ((long) (GetTimeInMillis() - pi->emulationTimeout) >= 0) {
                 pi->timeoutPending = FALSE;
+#ifndef KDRIVE_KBD
                 input_lock();
+#else
+                KdBlockSigio();
+#endif
                 KdReceiveTimeout(pi);
+#ifndef KDRIVE_KBD
                 input_unlock();
+#else
+                KdUnblockSigio();
+#endif
             }
         }
     }
@@ -2179,12 +2199,23 @@ KdCrossScreen(ScreenPtr pScreen, Bool entering)
 {
 }
 
+int KdCurScreen;                /* current event screen */
+
 static void
 KdWarpCursor(DeviceIntPtr pDev, ScreenPtr pScreen, int x, int y)
 {
+#ifndef KDRIVE_KBD
     input_lock();
+#else
+    KdBlockSigio();
+#endif
+    KdCurScreen = pScreen->myNum;
     miPointerWarpCursor(pDev, pScreen, x, y);
+#ifndef KDRIVE_KBD
     input_unlock();
+#else
+    KdUnblockSigio();
+#endif
 }
 
 miPointerScreenFuncRec kdPointerScreenFuncs = {

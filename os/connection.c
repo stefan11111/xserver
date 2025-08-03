914091634de03f50e2109b1d4b2b15500b790271
stefan11111 <stefan11111@shitposting.expert>
2025-08-03 22:30:32 +0300

    os: revert 2353d7a7105d4ed6dad1f215c11b8e4a0f85ef35
    This commit removes a function that will be used in another commit
    
    Signed-off-by: stefan11111 <stefan11111@shitposting.expert>

--- os/connection.c
+++ os/connection.c
@@ -640,7 +640,7 @@ EstablishNewConnections(int curconn, int ready, void *data)
 
     newconn = _XSERVTransGetConnectionNumber(new_trans_conn);
 
-    _XSERVTransNonBlock(new_trans_conn);
+    _XSERVTransSetOption(new_trans_conn, TRANS_NONBLOCKING, 1);
 
     if (trans_conn->flags & TRANS_NOXAUTH)
         new_trans_conn->flags = new_trans_conn->flags | TRANS_NOXAUTH;
@@ -1017,7 +1017,7 @@ AddClientOnOpenFD(int fd)
     if (ciptr == NULL)
         return FALSE;
 
-    _XSERVTransNonBlock(ciptr);
+    _XSERVTransSetOption(ciptr, TRANS_NONBLOCKING, 1);
     ciptr->flags |= TRANS_NOXAUTH;
 
     connect_time = GetTimeInMillis();

aac512ff8d4fa590a328935cd47438a28329e213
notbabaisyou <though-went-some-simple@proton.me>
2025-08-19 22:05:28 +0300

    hw/modesetting: Add drmmode_get_cursor_limit.
    Used in upcoming commit.

--- hw/xfree86/drivers/video/modesetting/drmmode_display.h
+++ hw/xfree86/drivers/video/modesetting/drmmode_display.h
@@ -355,4 +355,6 @@ Bool drmmode_crtc_get_fb_id(xf86CrtcPtr crtc, uint32_t *fb_id, int *x, int *y);
 void drmmode_set_dpms(ScrnInfoPtr scrn, int PowerManagementMode, int flags);
 void drmmode_crtc_set_vrr(xf86CrtcPtr crtc, Bool enabled);
 
+Bool drmmode_get_cursor_limit(ScrnInfoPtr pScrn, drmmode_cursor_dim_ptr cursor_lim);
+
 #endif

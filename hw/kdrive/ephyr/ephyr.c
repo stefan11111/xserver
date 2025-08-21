44ffe7d2a382aa7364404a71044e965312be4bb9
stefan11111 <stefan11111@shitposting.expert>
2025-08-21 19:49:29 +0300

    kdrive: fix warnings about missing prototypes and unused variables
    Signed-off-by: stefan11111 <stefan11111@shitposting.expert>

--- hw/kdrive/ephyr/ephyr.c
+++ hw/kdrive/ephyr/ephyr.c
@@ -667,6 +667,7 @@ ephyrSetGrabShortcut(char const* const desc)
     }
     else {
         const uint8_t fixed_bound = 255;
+        (void)fixed_bound;
         char buf[16];
         uint8_t j = 0;
         for (uint8_t i = 0;; ++i) {

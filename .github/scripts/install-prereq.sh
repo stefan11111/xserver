a8de19aca3d5bc249d072c57762c965462b240b2
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-26 14:14:44 +0200

    .github: use a smarter cloning script
    Several sites we need to clone our dependencies from (eg. freedesktop.org)
    have pretty unreliable servers, so our CI jobs often fail just because
    of temporary clone failure.
    
    Therefore adding a separate cloning script, which is more clever with automatic
    retries, but it also tries to keep the traffic low (eg. trying shallow clones
    if possible) and automatically detecting whether we're pulling a ref or a
    direct commit.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/install-prereq.sh
+++ .github/scripts/install-prereq.sh
@@ -9,17 +9,16 @@ cd $X11_BUILD_DIR
 
 build_meson   rendercheck       https://gitlab.freedesktop.org/xorg/test/rendercheck     rendercheck-1.6
 if [ "$X11_OS" = "Linux" ]; then
-build_meson   drm               https://gitlab.freedesktop.org/mesa/drm                  libdrm-2.4.121   "" \
-    -Domap=enabled
+build_meson   drm               https://gitlab.freedesktop.org/mesa/drm                  libdrm-2.4.121   -Domap=enabled
 fi
 build_meson   libxcvt           https://gitlab.freedesktop.org/xorg/lib/libxcvt          libxcvt-0.1.0
 build_ac      xorgproto         https://gitlab.freedesktop.org/xorg/proto/xorgproto      xorgproto-2024.1
 if [ "$X11_OS" = "Darwin" ]; then
 build_ac      xset              https://gitlab.freedesktop.org/xorg/app/xset             xset-1.2.5
 fi
-build_ac_xts  xts               https://gitlab.freedesktop.org/xorg/test/xts             master     aae51229af810efba24412511f60602fab53eded
+build_ac_xts  xts               https://gitlab.freedesktop.org/xorg/test/xts             aae51229af810efba24412511f60602fab53eded
 
-clone_source piglit             https://gitlab.freedesktop.org/mesa/piglit               main       265896c86f90cb72e8f218ba6a3617fca8b9a1e3
+clone_source piglit             https://gitlab.freedesktop.org/mesa/piglit               265896c86f90cb72e8f218ba6a3617fca8b9a1e3
 
 echo '[xts]' > piglit/piglit.conf
 echo "path=$X11_BUILD_DIR/xts" >> piglit/piglit.conf

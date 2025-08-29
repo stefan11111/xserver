6e2d5d3ddf41da77a1d8251a092df8b9bdf016bf
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-29 16:36:53 +0200

    render: use X_SEND_REPLY_SIMPLE() for simple replies
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- render/render.c
+++ render/render.c
@@ -256,11 +256,6 @@ static int
 ProcRenderQueryVersion(ClientPtr client)
 {
     RenderClientPtr pRenderClient = GetRenderClient(client);
-    xRenderQueryVersionReply rep = {
-        .type = X_Reply,
-        .sequenceNumber = client->sequence,
-        .length = 0
-    };
 
     REQUEST(xRenderQueryVersionReq);
 
@@ -269,24 +264,22 @@ ProcRenderQueryVersion(ClientPtr client)
     pRenderClient->major_version = stuff->majorVersion;
     pRenderClient->minor_version = stuff->minorVersion;
 
+    xRenderQueryVersionReply rep = {
+        .majorVersion = SERVER_RENDER_MAJOR_VERSION,
+        .minorVersion = SERVER_RENDER_MINOR_VERSION
+    };
+
     if ((stuff->majorVersion * 1000 + stuff->minorVersion) <
         (SERVER_RENDER_MAJOR_VERSION * 1000 + SERVER_RENDER_MINOR_VERSION)) {
         rep.majorVersion = stuff->majorVersion;
         rep.minorVersion = stuff->minorVersion;
     }
-    else {
-        rep.majorVersion = SERVER_RENDER_MAJOR_VERSION;
-        rep.minorVersion = SERVER_RENDER_MINOR_VERSION;
-    }
 
     if (client->swapped) {
-        swaps(&rep.sequenceNumber);
-        swapl(&rep.length);
         swapl(&rep.majorVersion);
         swapl(&rep.minorVersion);
     }
-    WriteToClient(client, sizeof(xRenderQueryVersionReply), &rep);
-    return Success;
+    return X_SEND_REPLY_SIMPLE(client, rep);
 }
 
 static VisualPtr

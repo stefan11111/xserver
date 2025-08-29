91e04e4ec60b00f884c3a2d752a4995ce452cb65
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-29 16:34:51 +0200

    record: use X_SEND_REPLY_SIMPLE() for simple replies
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- record/record.c
+++ record/record.c
@@ -1822,22 +1822,18 @@ ProcRecordQueryVersion(ClientPtr client)
 {
     /* REQUEST(xRecordQueryVersionReq); */
     xRecordQueryVersionReply rep = {
-        .type = X_Reply,
-        .sequenceNumber = client->sequence,
-        .length = 0,
         .majorVersion = SERVER_RECORD_MAJOR_VERSION,
         .minorVersion = SERVER_RECORD_MINOR_VERSION
     };
 
     REQUEST_SIZE_MATCH(xRecordQueryVersionReq);
     if (client->swapped) {
-        swaps(&rep.sequenceNumber);
         swaps(&rep.majorVersion);
         swaps(&rep.minorVersion);
     }
-    WriteToClient(client, sizeof(xRecordQueryVersionReply), &rep);
-    return Success;
-}                               /* ProcRecordQueryVersion */
+
+    return X_SEND_REPLY_SIMPLE(client, rep);
+}
 
 static int
 ProcRecordCreateContext(ClientPtr client)

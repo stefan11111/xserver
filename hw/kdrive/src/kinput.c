stefan11111/xserver

commit 97d72431cf455eba45c09014116f565679a4ad5c

kdrive: canonical `walkScreen` variable on screen list iterations
When iterating screen lists, consistently use the same variable name
`walkScreen` for holding current screen pointer everywhere.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

hw/kdrive/src/kinput.c
@@ -1924,7 +1924,6 @@ static Bool
 KdCursorOffScreen(ScreenPtr *ppScreen, int *x, int *y)
 {
     ScreenPtr pScreen = *ppScreen;
-    ScreenPtr pNewScreen;
     int n;
     int dx, dy;
     int best_x, best_y;
@@ -1947,11 +1946,11 @@ KdCursorOffScreen(ScreenPtr *ppScreen, int *x, int *y)
     n_best_y = -1;
     best_y = 32767;
     for (n = 0; n < screenInfo.numScreens; n++) {
-        pNewScreen = screenInfo.screens[n];
-        if (pNewScreen == pScreen)
+        ScreenPtr walkScreen = screenInfo.screens[n];
+        if (walkScreen == pScreen)
             continue;
-        dx = KdScreenOrigin(pNewScreen)->x - KdScreenOrigin(pScreen)->x;
-        dy = KdScreenOrigin(pNewScreen)->y - KdScreenOrigin(pScreen)->y;
+        dx = KdScreenOrigin(walkScreen)->x - KdScreenOrigin(walkScreen)->x;
+        dy = KdScreenOrigin(walkScreen)->y - KdScreenOrigin(walkScreen)->y;
         if (*x < 0) {
             if (dx < 0 && -dx < best_x) {
                 best_x = -dx;
@@ -1981,7 +1980,8 @@ KdCursorOffScreen(ScreenPtr *ppScreen, int *x, int *y)
         n_best_x = n_best_y;
     if (n_best_x == -1)
         return FALSE;
-    pNewScreen = screenInfo.screens[n_best_x];
+
+    ScreenPtr pNewScreen = screenInfo.screens[n_best_x];
 
     if (*x < 0)
         *x += pNewScreen->width;

hw/kdrive/ephyr/ephyr.c
@@ -958,8 +958,8 @@ screen_from_window(Window w)
     int i = 0;
 
     for (i = 0; i < screenInfo.numScreens; i++) {
-        ScreenPtr pScreen = screenInfo.screens[i];
-        KdPrivScreenPtr kdscrpriv = KdGetScreenPriv(pScreen);
+        ScreenPtr walkScreen = screenInfo.screens[i];
+        KdPrivScreenPtr kdscrpriv = KdGetScreenPriv(walkScreen);
         KdScreenInfo *screen = kdscrpriv->screen;
         EphyrScrPriv *scrpriv = screen->driver;
 

commit e2d9dc40543745f62424dfb5d33f02398590c4b6

kdrive: fix KdCursorOffScreen()
Bug slipped through: we have to substract the coordinates from the walked
screen to the current one, instead of the walked one from itself :o

Fixes: 97d72431cf455eba45c09014116f565679a4ad5c
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

hw/kdrive/src/kinput.c
@@ -2140,8 +2140,8 @@ KdCursorOffScreen(ScreenPtr *ppScreen, int *x, int *y)
         ScreenPtr walkScreen = screenInfo.screens[n];
         if (walkScreen == pScreen)
             continue;
-        dx = KdScreenOrigin(walkScreen)->x - KdScreenOrigin(walkScreen)->x;
-        dy = KdScreenOrigin(walkScreen)->y - KdScreenOrigin(walkScreen)->y;
+        dx = KdScreenOrigin(walkScreen)->x - KdScreenOrigin(pScreen)->x;
+        dy = KdScreenOrigin(walkScreen)->y - KdScreenOrigin(pScreen)->y;
         if (*x < 0) {
             if (dx < 0 && -dx < best_x) {
                 best_x = -dx;
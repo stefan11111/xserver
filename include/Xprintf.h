b3a5b5b61cf39bb472e86ffa79febb38ca7df1af
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-06 15:31:47 +0100

    os: drop unused Xscnprintf() and Xvscnprintf()
    Not used anywhere (not even in drivers), so no need to keep them anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- include/Xprintf.h
+++ include/Xprintf.h
@@ -55,16 +55,4 @@ extern _X_EXPORT int
 XNFvasprintf(char **ret, const char *_X_RESTRICT_KYWD fmt, va_list va)
 _X_ATTRIBUTE_PRINTF(2, 0);
 
-/*
- * These functions provide a portable implementation of the linux kernel
- * scnprintf & vscnprintf routines that return the number of bytes actually
- * copied during a snprintf, (excluding the final '\0').
- */
-extern _X_EXPORT int
-Xscnprintf(char *s, int n, const char * _X_RESTRICT_KYWD fmt, ...)
-_X_ATTRIBUTE_PRINTF(3,4);
-extern _X_EXPORT int
-Xvscnprintf(char *s, int n, const char * _X_RESTRICT_KYWD fmt, va_list va)
-_X_ATTRIBUTE_PRINTF(3,0);
-
 #endif                          /* XPRINTF_H */

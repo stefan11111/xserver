f24822c2b5db8563731af3bc38e96d8927a3c5bf
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-18 13:58:25 +0100

    rootless: drop obsolete support for internal server reset
    Not used anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- miext/rootless/rootlessWindow.c
+++ miext/rootless/rootlessWindow.c
@@ -61,13 +61,7 @@ extern Bool no_configure_window;
 
 #define DEFINE_ATOM_HELPER(func,atom_name)                      \
   static Atom func (void) {                                     \
-    static x_server_generation_t generation = 0;                \
-    static Atom atom;                                           \
-    if (generation != serverGeneration) {                       \
-      generation = serverGeneration;                            \
-      atom = dixAddAtom(atom_name);                             \
-    }                                                           \
-    return atom;                                                \
+    return dixAddAtom(atom_name);                               \
   }
 
 DEFINE_ATOM_HELPER(xa_native_window_id, "_NATIVE_WINDOW_ID")

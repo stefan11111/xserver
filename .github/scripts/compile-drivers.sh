99a9a67788152f0626790307cc8721b2cfa0b73c
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-20 13:49:01 +0100

    .github: also build dummy driver
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/compile-drivers.sh
+++ .github/scripts/compile-drivers.sh
@@ -19,6 +19,7 @@ build_drv_ac xf86-input-synaptics   $X11L_DRV_GIT/xf86-input-synaptics   xlibre-
 
 build_drv_ac xf86-video-amdgpu      $X11L_DRV_GIT/xf86-video-amdgpu      xlibre-xf86-video-amdgpu-23.0.0.5
 build_drv_ac xf86-video-ati         $X11L_DRV_GIT/xf86-video-ati         xlibre-xf86-video-ati-22.0.0.3
+build_drv_ac xf86-video-dummy       $X11L_DRV_GIT/xf86-video-dummy       xlibre-xf86-video-dummy-0.4.1.3
 build_drv_ac xf86-video-intel       $X11L_DRV_GIT/xf86-video-intel       xlibre-xf86-video-intel-3.0.0.3
 build_drv_ac xf86-video-nouveau     $X11L_DRV_GIT/xf86-video-nouveau     xlibre-xf86-video-nouveau-1.0.18.3
 build_drv_ac xf86-video-omap        $X11L_DRV_GIT/xf86-video-omap        xlibre-xf86-video-omap-0.4.5.2

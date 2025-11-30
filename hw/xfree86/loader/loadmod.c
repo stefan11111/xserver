3b99373a17e799974dd5d8fc41e161626398161f
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2025-11-30 23:13:39 +0100

    Fixed a out of bounds read.
    Signed-off-by: Branimir Ri\v{c}ko <rickobranimir@gmail.com>

--- hw/xfree86/loader/loadmod.c
+++ hw/xfree86/loader/loadmod.c
@@ -788,7 +788,7 @@ LoadModule(const char *module, void *options, const XF86ModReqInfo *modreq,
     LogMessageVerb(X_INFO, 3, "LoadModule: \"%s\"", module);
 
     /* Ignore abi check for the nvidia proprietary DDX driver */
-    is_nvidia_proprietary = !memcmp(module, "nvidia", sizeof("nvidia"));
+    is_nvidia_proprietary = !strcmp(module, "nvidia");
 
     patterns = InitPatterns(NULL);
     name = LoaderGetCanonicalName(module, patterns);

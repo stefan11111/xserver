e11e90cab6b8f5f70cce3aa4860eb473007b425e
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-16 18:05:11 +0100

    .github: compile-drivers.sh: add xf86-video-v4l driver
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/compile-drivers.sh
+++ .github/scripts/compile-drivers.sh
@@ -60,6 +60,7 @@ build_xf86drv_ac    video-tdfx              1.5.0.2
 build_xf86drv_ac    video-trident           1.4.0.2
 build_xf86drv_ac    video-vesa              2.6.0.2
 build_xf86drv_ac    video-vmware            13.4.0.3
+build_xf86drv_ac    video-v4l               0.3.0.2
 build_xf86drv_ac    video-vbox              1.0.1.2
 build_xf86drv_ac    video-voodoo            1.2.6.3
 build_xf86drv_ac    video-xgi               1.6.1.3

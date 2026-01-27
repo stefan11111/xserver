769616256fb9db52046ae7c9c27f628878e81d03
Enrico Weigelt, metux IT consult <info@metux.net>
2026-01-27 17:29:43 +0100

    xfree86: ddc: drop unused IS_INTERLACED() macro
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/ddc/edid.h
+++ hw/xfree86/ddc/edid.h
@@ -330,7 +330,6 @@
 #define CVT_SUPPORTED(x) (x & 0x1)
 
 /* detailed timing misc */
-#define IS_INTERLACED(x)  (x)
 #define IS_STEREO(x)  (x)
 #define IS_RIGHT_STEREO(x) (x & 0x01)
 #define IS_LEFT_STEREO(x) (x & 0x02)

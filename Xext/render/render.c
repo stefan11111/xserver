4e2bb6d1da29d669ee1498225874d1a3100cca4f
Enrico Weigelt, metux IT consult <info@metux.net>
2026-05-08 18:10:16 +0200

    render: use X_REQUEST_*() macros in ProcRenderTrapezoids()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/render/render.c
+++ Xext/render/render.c
@@ -2653,17 +2653,13 @@ ProcRenderComposite(ClientPtr client)
 static int
 ProcRenderTrapezoids(ClientPtr client)
 {
-    REQUEST(xRenderTrapezoidsReq);
-    REQUEST_AT_LEAST_SIZE(xRenderTrapezoidsReq);
-
-    if (client->swapped) {
-        swapl(&stuff->src);
-        swapl(&stuff->dst);
-        swapl(&stuff->maskFormat);
-        swaps(&stuff->xSrc);
-        swaps(&stuff->ySrc);
-        SwapRestL(stuff);
-    }
+    X_REQUEST_HEAD_AT_LEAST(xRenderTrapezoidsReq);
+    X_REQUEST_FIELD_CARD32(src);
+    X_REQUEST_FIELD_CARD32(dst);
+    X_REQUEST_FIELD_CARD32(maskFormat);
+    X_REQUEST_FIELD_CARD16(xSrc);
+    X_REQUEST_FIELD_CARD16(ySrc);
+    X_REQUEST_REST_CARD32();
 
 #ifdef XINERAMA
     return (usePanoramiX ? PanoramiXRenderTrapezoids(client, stuff)

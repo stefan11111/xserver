5c2d4aa0a952046d8687f5aaa45e76716e7d3320
Enrico Weigelt, metux IT consult <info@metux.net>
2026-05-12 13:38:35 +0200

    .github: move refs for common dependencies into conf.sh
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/install-prereq-drivers.sh
+++ .github/scripts/install-prereq-drivers.sh
@@ -8,6 +8,6 @@ mkdir -p $X11_BUILD_DIR
 cd $X11_BUILD_DIR
 
 if [ "$X11_OS" = "Linux" ]; then
-build_meson   drm               $(fdo_mirror drm)                          libdrm-2.4.121   -Domap=enabled -Dfreedreno=enabled
+build_meson   drm               $(fdo_mirror drm)                          $PKG_LIBDRM_REF   -Domap=enabled -Dfreedreno=enabled
 fi
-build_meson   xorgproto         $(fdo_mirror xorgproto)                    xorgproto-2024.1
+build_meson   xorgproto         $(fdo_mirror xorgproto)                    $PKG_XORGPROTO_REF

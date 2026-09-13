91e40812c7c047a44bf3d664c9e339d78b9e32c5
stefan11111 <stefan11111github@gmail.com>
2026-09-14 01:11:59 +0300

    kdrive/glamor: Add `-nogbm` option
    Forces glamor to not use libgbm
    
    Signed-off-by: stefan11111 <stefan11111github@gmail.com>

--- hw/kdrive/src/kglamor.c
+++ hw/kdrive/src/kglamor.c
@@ -194,6 +194,11 @@ KdGlamorParse(KdGlamorInfo *info, int argc, char **argv, int i)
         return 1;
     }
 
+    if (!strcmp(argv[i], "-nogbm")) {
+        info->use_gbm = FALSE;
+        return 1;
+    }
+
     if (!strcmp(argv[i], "-direct-dri3")) {
         info->direct_dri3 = TRUE;
         return 1;
@@ -248,6 +253,8 @@ KdGlamorUseMsg(void)
         ("-noglamor            Force disable glamor render acceleration\n");
     ErrorF
         ("-gbm                 Allow glamor to use libgbm\n");
+    ErrorF
+        ("-nogbm               Force glamor to not use libgbm\n");
     ErrorF
         ("-direct-dri3         Force glamor to use the direct DRI3 implementation\n");
     ErrorF
